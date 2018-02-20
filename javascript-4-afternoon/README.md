624e10d3f02e637a2c84e6067ec52907b54f0405
Steve Smith <stevesmithsays@Steves-MacBook-Pro.local>
2018-02-20 09:38:32 -0600

    commited afternoon project

---
+++ javascript-4-afternoon/README.md
@@ -0,0 +1,31 @@
+<img src="https://devmounta.in/img/logowhiteblue.png" width="250" align="right">
+
+# Project Summary
+
+In this project, we'll provide practice JavaScript problems to help you better understand `closures` and `constructors`.
+
+## Setup
+
+* `Fork` this repository.
+* `Clone` your fork.
+* Open `./index.html` with your browser.
+
+## Directions
+
+Complete all of the activities inside of `closures.js` and `constructors.js`. Use their respective `html` files to see if you can make all the tests pass. Remember to commit and push your code often. Good luck!
+
+## Tips and Tricks
+
+One of the biggest tools at the developer's disposal is the browser's built-in debugger. To use it, open `./index.html` with your browser. Another great tool for small and isolated pieces of code is <a href="http://www.pythontutor.com/visualize.html#">Pyton Tutor</a>. Just make sure to change the code to `JavaScript ES6`.
+
+## Contributions
+
+If you see a problem or a typo, please fork, make the necessary changes, and create a pull request so we can review your changes and merge them into the master repo and branch.
+
+## Copyright
+
+© DevMountain LLC, 2017. Unauthorized use and/or duplication of this material without express and written permission from DevMountain, LLC is strictly prohibited. Excerpts and links may be used, provided that full and clear credit is given to DevMountain with appropriate and specific direction to the original content.
+
+<p align="center">
+<img src="https://devmounta.in/img/logowhiteblue.png" width="250">
+</p>
---
+++ javascript-4-afternoon/constructors.js
@@ -0,0 +1,72 @@
+/* 
+  Once you complete a problem, refresh ./constructors.html in your browser and check to see if the problem's test(s) are passing.
+  Passed tests will be indicated by a green circle.
+  Failed tests will be indicated by a red X.
+
+  You can refresh the page at any time to re-run all the tests.
+*/
+
+
+////////// PROBLEM 1 //////////
+
+/*
+  Create a constructor function called CarFactory that will build car objects with make and model properties.
+  The values for the make and model will come from two parameters, make and model (in that order).
+*/
+
+function CarFactory(make, model){
+  this.make = make;
+  this.model = model;
+};
+
+let coolCar = new CarFactory('Volvo', '240');
+
+////////// PROBLEM 2 //////////
+
+// Do not edit the code below.
+function Employee(name, email, hireDate) {
+  this.name = name;
+  this.email = email;
+  this.hireDate = hireDate;
+}
+
+// Do not edit the code above.
+
+/* 
+  Invoke the constructor function above and pass in 'Bob', 'bob@gmail.com', and '01-02-98' as arguments.
+  Assign the result of the invocation to a variable called bob.
+*/
+
+let bob = new Employee('Bob', 'bob@gmail.com', '01-02-98');
+
+
+
+////////// PROBLEM 4 //////////
+
+// Do not edit the code below.
+var prius = new Car('Toyota', 'Prius', 2011);
+var mustang = new Car('Ford', 'Mustang', 2013);
+prius.moveCar(); // Increments prius' move property by 10. Returns the new move property.
+mustang.moveCar(); // Increments mustang' move property by 10. Returns the new move property.
+
+// Do not edit the code above.
+
+/*
+  Write a constructor function, including method definitions, which will make the above function invocations function properly.
+
+  Hint: you'll need to add a move property, with a starting value of zero, and write a moveCar function which will increment the move property by 10.
+  The move property will be added to every object that is being returned from the Car function.
+  You'll also need to use the 'this' keyword properly in order to make sure you're invoking moveCar on the right object (prius vs mustang).
+*/
+
+function Car(make, model, year){
+  let move = 0;
+  this.make = make;
+  this.model = model;
+  this.year = year;
+
+  this.moveCar = function (){
+    return move += 10;
+  }
+}
+
